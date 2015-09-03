d931ed39faf29cb116d3a9af0cddb9ebb861500c
Goran Ehrsson <goran@technipelago.se>
2015-09-03 13:34:42 +0200

    Added navigation item ordering

--- grails-app/conf/CrmContactNavigation.groovy
+++ grails-app/conf/CrmContactNavigation.groovy
@@ -1,9 +1,9 @@
 navigation = {
     main(global: true) {
-        crmContact controller: 'crmContact', action: 'index', data: [icon: 'person']
+        crmContact controller: 'crmContact', action: 'index', order: 10, data: [icon: 'person']
     }
     admin(global: true) {
-        crmAddressType controller: 'crmAddressType', action: 'index', data: [icon: 'envelope']
-        crmContactCategoryType controller: 'crmContactCategoryType', action: 'index', data: [icon: 'person']
+        crmAddressType controller: 'crmAddressType', action: 'index', order: 110, data: [icon: 'envelope']
+        crmContactCategoryType controller: 'crmContactCategoryType', action: 'index', order: 120, data: [icon: 'person']
     }
 }
